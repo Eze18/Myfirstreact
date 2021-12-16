fa5f76d0c81df983dcf11b97d2e9134837ade3b5
Eze18 <90942548+Eze18@users.noreply.github.com>
2021-12-17 00:38:09 +0800

    git add --all

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Message from './components/Message';
+import Button from './components/Button';
 
+//jsx - js xml 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. ----Ezekiel
+           <code>Get that 30</code> Get that 30 - Ezekiel
         </p>
+    
+        <Greet name= "Astartes" Spacemarine= "Guilliman">
+           <Message/>
+        </Greet>
+        <Greet name= "Emperor" Spacemarine= "of Mankind"/>
+           <Button/>
+        <Greet name= "" Native rambo= "catachan"/>
+        <Greet name= "Corvus" Spacemarine= "Eldar"/>
         <a
           className="App-link"
           href="https://reactjs.org"
